643ff28b221f91ee1578a9ae499db7920a31c5c0
daveflan <daveflan@gmail.com>
2020-04-28 08:04:59 +0100

    Uploaded Test project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VaultexModels;
 
 namespace VaultexData.Controllers
 {
@@ -10,21 +11,12 @@ namespace VaultexData.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            string organisationnumber = "";
+
+            var model = new EmployeeOrganisationDataViewModel(organisationnumber);
+            
+            return View(model);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
